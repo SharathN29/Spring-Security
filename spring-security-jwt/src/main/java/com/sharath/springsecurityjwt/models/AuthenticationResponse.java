68d0c45ef2fd7d5e1a8b3c64ecb096e1da4827b4
sharathn29 <nsharathn@gmail.com>
2020-06-11 12:01:07 -0400

    Spring Boot project with JWT implementation

---
+++ spring-security-jwt/src/main/java/com/sharath/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.sharath.springsecurityjwt.models;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
