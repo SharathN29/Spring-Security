69db468929eeb8c09e39e63c50d6de3f69c911ed
sharathn29 <nsharathn@gmail.com>
2020-06-10 23:27:35 -0400

    Spring Security with JPA

---
+++ spring-security-jpa/src/main/java/com/sharath/springsecurityjpa/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.sharath.springsecurityjpa;
+
+import com.sharath.springsecurityjpa.models.MyUserDetails;
+import com.sharath.springsecurityjpa.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service("userDetailsService")
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found " + userName));
+        return user.map(MyUserDetails::new).get();
+    }
+}
