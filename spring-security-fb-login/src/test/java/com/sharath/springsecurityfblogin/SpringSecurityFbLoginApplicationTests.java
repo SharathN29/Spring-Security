f5149f2eb351e13f1b457b3d18d4cbe9677cc736
sharathn29 <nsharathn@gmail.com>
2020-06-11 17:57:13 -0400

    Spring security with OAuth - FB

---
+++ spring-security-fb-login/src/test/java/com/sharath/springsecurityfblogin/SpringSecurityFbLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.springsecurityfblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFbLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
